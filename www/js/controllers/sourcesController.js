31a757c1b6c800c16993ec7fd88225a282f2bddb
Sanette Tanaka <sanette.tanaka@gmail.com>
2015-07-16 08:27:42 -0600

    Add select into note import

--- www/js/controllers/sourcesController.js
+++ www/js/controllers/sourcesController.js
@@ -8,4 +8,17 @@ angular.module('leder.sourcesController', [])
   		$scope.sourceNotes = notetitles;
   });
 
+  $scope.saveSourceNotes = function() {
+  	console.log("working");
+  	for (var i=0; i < $scope.sourceNotes.length; i++) {
+
+  		if ($scope.sourceNotes[i].touched) {
+  			console.log("Note has been tagged");
+  			console.log($scope.sourceNotes[i]);
+  		} else {
+  			console.log("not tagged :(");
+  		}
+  	};
+  }
+
 })
